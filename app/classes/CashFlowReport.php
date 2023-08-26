b6e7d7a3d554716c51106e5f861f52f6b6721ebb
Vitor Moreira de Oliveira <vitormoroli@gmail.com>
2023-08-26 20:09:40 -0300

    formatando os valores para o relatório

--- app/classes/CashFlowReport.php
+++ app/classes/CashFlowReport.php
@@ -2,6 +2,8 @@
 
 namespace app\classes;
 
+use function PHPSTORM_META\type;
+
 class CashFlowReport{
 
     /** 
@@ -20,9 +22,26 @@ class CashFlowReport{
         $report['max_value'] = $max[1];
         $report['loss'] = $this->loss($profit_month);
         $report['profit_percent'] = $this->profitPercent($data,$report['profit_year']);
+        return $this->formatValues($report);
+    }
+
+    /**
+     * @param array
+     * @return array
+     */
+    private function formatValues($report)
+    {
+        $report['profit_year'] = number_format($report['profit_year'],2,',','.');
+        $report['profit_mean'] = number_format($report['profit_mean'],2,',','.');
+        $report['max_value'] = number_format($report['max_value'],2,',','.');
+        $report['profit_percent'] = number_format($report['profit_percent'],2,',','.');
         return $report;
     }
 
+    /**
+     * @param array
+     * @return array
+     */
     private function profitMonth($data){
         unset($data[0]);
 
@@ -35,21 +54,32 @@ class CashFlowReport{
         return $profit_month;
     }
 
+    /**
+     * @param array
+     * @return int|float
+     * 
+     */
     private function profitYear($profit_month){
-        $profit_year = array_reduce($profit_month,function($carry,$value){
+        return array_reduce($profit_month,function($carry,$value){
             $carry += $value;
             return $carry;
         },0);
-
-        return round($profit_year,2);
     }
 
+    /**
+     * @param int|float
+     * @return int|float
+     */
     private function profitMean($profit_year){
-        return round(($profit_year/12),2);
+        return ($profit_year/12); 
     }
 
+    /**
+     * @param array
+     * @return array
+     */
     private function max($profit_month){
-        $max_value = round(max($profit_month),2);
+        $max_value = max($profit_month);
 
         $max_month = array_keys(array_filter($profit_month, function ($element) use ($max_value) { 
             return $element == $max_value; 
@@ -61,9 +91,13 @@ class CashFlowReport{
         else{
             $max_month = $max_month[0];
         }
-        return [$max_month,round($max_value,2)];
+        return [$max_month,$max_value];
     }
 
+    /**
+     * @param array
+     * @return string
+     */
     private function loss($profit_month){
         $loss = array_filter($profit_month,function($element){
             return $element < 0;
@@ -81,12 +115,17 @@ class CashFlowReport{
         return $loss;
     }
 
+    /**
+     * @param array
+     * @param int|float
+     * @return int|float
+     */
     private function profitPercent($data,$profit_year){
         $annual_expense = array_reduce($data,function($carry,$element){
             $carry += (float)$element[1];
             return $carry;
         },0);
 
-        return round(($profit_year/$annual_expense)*100,2);
+        return ($profit_year/$annual_expense)*100;
     }
 }
